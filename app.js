c8bcb53b939d49c994b230dc3d331d0fb3f75242
sahil ubhe <sahilubhe1150@gmail.com>
2021-06-07 21:45:07 +0530

    First Message. First code

---
+++ app.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+const submit = document.querySelector('#submit');
+
+//Display Mobile Menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
